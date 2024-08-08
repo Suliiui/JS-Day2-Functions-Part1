d22d79ccc9383c81c4e0c268e0a44afcb50ed0d5
Sulaiman Al Refai <sulaimanmarwan54@gmail.com>
2024-08-09 02:13:36 +0300

    complete

---
+++ functions1.js
@@ -0,0 +1,27 @@
+
+function firstName(name){
+    console.log(name)
+}
+
+firstName("Sulaiman");
+
+function printAge(Age){
+    console.log(Age);
+}
+printAge(2024-2006);
+complete=("Sulaiman")
+function printNameAndAge(by, n){
+        const Age = 2024-by
+    console.log("Hello " + n + " you are " + Age + " years old")
+}
+printNameAndAge(2006, "Sulaiman")
+
+function printHello(name, language) {
+    if (language === 'es'){
+        console.log(`Hola ${name}`);
+}else if (language === 'fr') {
+    console.log(`Bonjour ${name}`)}
+    else if (language === 'tr') {console.log(`Merhaba ${name}`)}
+    else console.log(`Hello ${name}`);
+}
+printHello("Sulaiman", "es")
